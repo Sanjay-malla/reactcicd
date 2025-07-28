36a6654bf15e857e7ca525525b51412e41956b67
Sanjay-malla <mallasanjay200@gmail.com>
2025-07-28 10:34:55 +0530

    Delete src/App.test.jsx

--- src/App.test.jsx
+++
@@ -1,8 +0,0 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
-// import '@testing-library/jest-dom';
-
-// test('renders App component', () => {
-//   render(<App />);
-//   expect(screen.getByText(/hello/i)).toBeInTheDocument();
-// });
